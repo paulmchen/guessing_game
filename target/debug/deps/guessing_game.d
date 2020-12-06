6914c24b179d6d6ac91a78420ebc63eb4466bbae
mingchen <chenp02@gmail.com>
2020-12-06 14:18:16 -0500

    remove target

--- target/debug/deps/guessing_game.d
+++
@@ -1,5 +0,0 @@
-/Users/mingchen/rust-workspace/projects/guessing_game/target/debug/deps/guessing_game: src/main.rs
-
-/Users/mingchen/rust-workspace/projects/guessing_game/target/debug/deps/guessing_game.d: src/main.rs
-
-src/main.rs:
