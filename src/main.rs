8abe24d99300a5db34d5740ad5bdcdce08a584ed
mingchen <chenp02@gmail.com>
2021-01-01 15:55:55 -0500

    check valid guess number range

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,24 @@
 use rand::Rng;
+
 use std::cmp::Ordering;
 use std::io;
 
-// guess a number
-fn guess_number(lower: u32, higher:u32) {
-    let secret_number = rand::thread_rng().gen_range(lower, higher+1);
-    let mut attempt:u32 = 0;
+// Guess a number
+// The range of a number to be guessed is between values of 'lower' and 'higher' parameters specified
+fn guess_number(lower: u32, higher: u32) {
+    if lower > higher {
+        println!("Incorrect number range specified.");
+        return;
+    }
+
+    let secret_number = rand::thread_rng().gen_range(lower, higher + 1);
+    let mut attempt: u32 = 0;
 
     loop {
-        println!("Please input your guess (a number between 1 and 100)：");
+        println!(
+            "Please input your guess (a number between {} and {})：",
+            lower, higher
+        );
         let mut guess = String::new();
         io::stdin()
             .read_line(&mut guess)
@@ -33,8 +43,7 @@ fn guess_number(lower: u32, higher:u32) {
     }
 }
 
-
 fn main() {
     println!("Guess the number!");
-    guess_number(1,100);
+    guess_number(1, 100);
 }
